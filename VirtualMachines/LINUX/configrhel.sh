da09537d7692a0ef18583aeccdc88337b19e1a25
McEldon <mmceldon@deloitte.com>
2018-10-29 06:44:58 -0400

    SETTINGS BASE64 Cracked

--- VirtualMachines/LINUX/configrhel.sh
+++ VirtualMachines/LINUX/configrhel.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sudo rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-sudo yum install nginx -y
-sudo systemctl start nginx.service
+sudo yum -y install httpd
+sudo apachectl start

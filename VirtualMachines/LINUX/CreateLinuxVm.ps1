14d159565f06901f9475f93287d6a37a1217ad60
McEldon <mmceldon@deloitte.com>
2018-10-29 03:26:10 -0400

    UPDATE

--- VirtualMachines/LINUX/CreateLinuxVm.ps1
+++ VirtualMachines/LINUX/CreateLinuxVm.ps1
@@ -8,7 +8,7 @@ param(
  $resourceGroupName = "rg-lab-dev",
 
  [string]
- $templateFilePath = "template.json",
+ $templateFilePath = "template.ext.json",
 
  [string]
  $parametersFilePath = "parameters.json"
@@ -17,14 +17,14 @@ param(
 
 Clear-Host
 Write-Output("SCRIPT START")
-
+# https://salabops01.blob.core.windows.net/vmscripts/configrhel.sh
 #region ## FUNCTIONS ##
 function CreateVm(){
     
     Write-Host "Starting deployment...";
     
     if(Test-Path $parametersFilePath) {
-        Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
+        New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
     } else {
         Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
     }
--- VirtualMachines/LINUX/configrhel.sh
+++ VirtualMachines/LINUX/configrhel.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sudo rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-sudo yum install nginx
+sudo yum install nginx -y
 sudo systemctl start nginx.service
-sudo systemctl status nginx.service
